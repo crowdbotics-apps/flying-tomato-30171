23dd885fcacca2a9e4b62a30b621b652a1a5d0ac
Crowdbotics <team@crowdbotics.com>
2021-08-29 04:27:09 +0000

    Initial commit.

---
+++ android/app/src/main/java/com/flying_tomato_30171/MainActivity.java
@@ -0,0 +1,15 @@
+package com.flying_tomato_30171;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+  /**
+   * Returns the name of the main component registered from JavaScript. This is used to schedule
+   * rendering of the component.
+   */
+  @Override
+  protected String getMainComponentName() {
+    return "flying_tomato_30171";
+  }
+}
---
+++ options/options.js
@@ -0,0 +1,6 @@
+export const globalOptions = {
+  name: "flying_tomato_30171Identifier",
+  url: "https://flying_tomato_30171Identifier.botics.co",
+  api: "https://flying_tomato_30171Identifier.botics.co/api/v1"
+}
+export const modulesOptions = {}
